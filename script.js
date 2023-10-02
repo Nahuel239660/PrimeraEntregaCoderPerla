e2f4228975d618c0c1f5c8f039ad3d6ae67d92f8
Nahuel Perla <92473194+Nahuel239660@users.noreply.github.com>
2023-10-02 12:31:37 -0300

    arreglo de errores

--- script.js
+++ script.js
@@ -28,7 +28,6 @@ const vehiculos = [
   new Vehiculo("Audi"),
 ];
 
-// Elementos select
 const selectMarca = document.getElementById("marca");
 const selectModelo = document.getElementById("modelo");
 const selectAnio = document.getElementById("anio");
@@ -53,7 +52,7 @@ const preciosBase = {
   Peugeot: 17000,
 };
 
-// Opciones de año para cada marca y modelo
+// constante que almacena las opciones de año para cada marca y modelo
 const opcionesAnio = {
   Chevrolet: {
     Agile: [2020, 2021, 2022],
@@ -69,11 +68,13 @@ const opcionesAnio = {
   },
 };
 
-// Función para llenar el select de año y modelo dependiendo de la marca
+// Función para completar año y modelo dependiendo la marca
 function llenarSelectAnio(marca, modelo) {
   const opciones = opcionesAnio[marca][modelo] || [];
+  // Limpiar las opciones anteriores
   selectAnio.innerHTML = '<option value="">Año...</option>';
 
+  // Llenar el select con las opciones de año
   opciones.forEach((anio) => {
     const option = document.createElement("option");
     option.value = anio;
@@ -82,77 +83,105 @@ function llenarSelectAnio(marca, modelo) {
   });
 }
 
-// Event listener para el cambio en el select de marca
 selectMarca.addEventListener("change", () => {
+  // Habilitar el elemento selectModelo
   selectModelo.disabled = false;
+
   const marcaSeleccionada = selectMarca.value;
 
-  if (marcaSeleccionada in opcionesAnio) {
-    llenarSelectAnio(marcaSeleccionada, selectModelo.value);
-  } else {
-    selectAnio.innerHTML = '<option value="">Año...</option>';
-    selectAnio.disabled = true;
+  // opciones de modelo según la marca seleccionada
+  if (marcaSeleccionada === "Chevrolet") {
+    selectModelo.innerHTML = `
+      <option value="">Elegir</option>
+      <option value="Agile">Agile</option>
+      <option value="Camaro">Aveo</option>
+    `;
+  } else if (marcaSeleccionada === "Ford") {
+    selectModelo.innerHTML = `
+      <option value="">Elegir</option>
+      <option value="Ranger">EcoSport</option>
+      <option value="Escape">Escape</option>
+    `;
+  } else if (marcaSeleccionada === "Peugeot") {
+    selectModelo.innerHTML = `
+      <option value="">Elegir</option>
+      <option value="208">208</option>
+      <option value="2008">2008</option>
+    `;
   }
+
+  // Llenar el select de año dependiendo la marca y modelo seleccionado
+  llenarSelectAnio(marcaSeleccionada, selectModelo.value);
 });
 
-// Event listener para el cambio en el select de modelo
+// Agregar un cambio al elemento selectModelo
 selectModelo.addEventListener("change", () => {
+  // Habilitar el elemento selectAnio
+  selectAnio.disabled = false;
+
   const marcaSeleccionada = selectMarca.value;
   const modeloSeleccionado = selectModelo.value;
 
-  if (marcaSeleccionada in opcionesAnio) {
-    llenarSelectAnio(marcaSeleccionada, modeloSeleccionado);
-  } else {
-    selectAnio.innerHTML = '<option value="">Año...</option>';
-    selectAnio.disabled = true;
-  }
+  // Llenar el select de año en función de la marca y el modelo seleccionados
+  llenarSelectAnio(marcaSeleccionada, modeloSeleccionado);
 });
 
-// Event listener para el botón "Siguiente" en la primera pestaña
+// Función para mostrar la segunda pestaña al hacer clic en el botón "siguiente" en la primera pestaña
 document.getElementById("botonSiguiente").addEventListener("click", () => {
+  // Ocultar la primera pestaña
   document.getElementById("home-tab-pane").classList.remove("show", "active");
+  // Quitar la clase "active" del botón de la primera pestaña
   document.getElementById("home-tab").classList.remove("active");
-
+  
+  // Mostrar la segunda pestaña
   document.getElementById("profile-tab-pane").classList.add("show", "active");
+  // Agregar la clase "active" al botón de la segunda pestaña
   document.getElementById("profile-tab").classList.add("active");
 });
 
-// Event listener para el botón "Siguiente" en la segunda pestaña
+
+// Función para mostrar la tercer pestaña al hacer clic en el botón "siguiente" en la segunda pestaña
 document.getElementById("botonSiguiente2").addEventListener("click", (e) => {
   e.preventDefault();
-
+  // Ocultar la segunda pestaña
   document.getElementById("profile-tab-pane").classList.remove("show", "active");
+  // Quitar la clase "active" del botón de la segunda pestaña
   document.getElementById("profile-tab").classList.remove("active");
 
+  // Mostrar la tercera pestaña
   document.getElementById("contact-tab-pane").classList.add("show", "active");
+  // Agregar la clase "active" al botón de la tercera pestaña
   document.getElementById("contact-tab").classList.add("active");
   mostrarPerfil1();
 });
 
-// Función para mostrar la segunda pestaña
+
 function mostrarPerfil1() {
+  // Obtener los valores ingresados por el usuario
   nombreGuardado = document.getElementById("nombre").value;
   apellidoGuardado = document.getElementById("apellido").value;
   dniGuardado = document.getElementById("dni").value;
   correoGuardado = document.getElementById("correo").value;
 
+  // Verificar si se han completado los datos antes de avanzar
   if (nombreGuardado && apellidoGuardado && dniGuardado && correoGuardado) {
+    // Habilitar y activar la pestaña "Costo"
     document.getElementById("contact-tab").removeAttribute("disabled");
     document.getElementById("contact-tab").classList.add("active");
     document.getElementById("contact-tab-pane").classList.add("show", "active");
 
+    // Calcular la cotización
     calcularCotizacion();
   } else {
+    // Mostrar un mensaje de error si no se completaron los datos
     alert("Por favor, complete todos los campos antes de continuar.");
   }
 }
 
-// Event listener para el botón "Contratar"
 document.getElementById("botonContratar").addEventListener("click", () => {
   alert("¡Contratación exitosa!");
 });
 
-// Función para calcular la cotización
 function calcularCotizacion() {
   let resultadoHTML = "<h2>Costo:</h2>";
   resultadoHTML += "<p>Nombre: " + nombreGuardado + "</p>";
@@ -160,27 +189,35 @@ function calcularCotizacion() {
   resultadoHTML += "<p>DNI: " + dniGuardado + "</p>";
   resultadoHTML += "<p>Correo: " + correoGuardado + "</p>";
 
+  // traer los valores seleccionados de los elementos <select>
   const marca = selectMarca.value;
   const modelo = selectModelo.value;
   const anio = selectAnio.value;
 
+  resultadoHTML += "<p>Marca: " + marca + "</p>";
+  resultadoHTML += "<p>Modelo: " + modelo + "</p>";
+  resultadoHTML += "<p>Año: " + anio + "</p>";
+
+  // precio base según la marca seleccionada
   let precioBase = preciosBase[marca] || 0;
 
-  if (modelo === "Agile") {
-    precioBase += 1000;
-  } else if (modelo === "Aveo") {
-    precioBase += 1200;
+  // Agregar precios adicionales 
+  if (modelo === "Camaro") {
+    precioBase += 2000; 
+  } else if (modelo === "Agile") {
+    precioBase += 1200; 
   }
 
+  // Agregar precios adicionales según el año 
   if (anio === "2022") {
-    precioBase += 800;
+    precioBase += 800; 
   } else if (anio === "2021") {
-    precioBase += 600;
+    precioBase += 600; 
   }
 
   resultadoHTML += "<p>Precio Total: $" + precioBase + "</p>";
 
-  const resultadoElement = document.getElementById("resultado");
+  let resultadoElement = document.getElementById("resultado");
   resultadoElement.innerHTML = resultadoHTML;
 }
 
